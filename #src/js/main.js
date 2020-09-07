e85ee79e82d71411ecce8e3818605afcd882feef
avp-dw <ya.stily@gmail.com>
2020-09-07 16:02:46 +0500

    added menu, booking, discover, footer, adaptive

--- #src/js/main.js
+++ #src/js/main.js
@@ -21,3 +21,16 @@ $(document).ready(function () {
 });
 
 // for webP
+
+
+const anchors = document.querySelectorAll('a[href*="#"]')
+for (let anchor of anchors) {
+  anchor.addEventListener("click", function(event) {
+    event.preventDefault();
+    const blockID = anchor.getAttribute('href')
+    document.querySelector('' + blockID).scrollIntoView({
+      behavior: "smooth",
+      block: "start"
+    })
+  })
+};
